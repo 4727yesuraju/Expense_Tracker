1f6f021a988b451a64adf8616481f5bfa4349864
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-07-21 08:19:22 +0530

    concurrently is added

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -2,68 +2,243 @@ import React, { useState } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
-function PieChart() {
-    const UserData = [
-        {
-          id: 1,
-          year: 2016,
-          userGain: 80000,
-          userLost: 823,
-        },
-        {
-          id: 2,
-          year: 2017,
-          userGain: 45677,
-          userLost: 345,
-        },
-        {
-          id: 3,
-          year: 2018,
-          userGain: 78888,
-          userLost: 555,
-        },
-        {
-          id: 4,
-          year: 2019,
-          userGain: 90000,
-          userLost: 4555,
-        },
-        {
-          id: 5,
-          year: 2020,
-          userGain: 4300,
-          userLost: 234,
-        },
-      ];
+function PieChart({expenses}) {
+    console.log(expenses)
+
+    const totalExpenses = expenses;
+    const dailyExpenses = expenses.filter(expense=>expense.period=='daily');
+    const weeklyExpenses = expenses.filter(expense=>expense.period=='weekly');
+    const monthlyExpenses = expenses.filter(expense=>expense.period=='monthly');
+    const Expenses = {
+        totalExpenses,dailyExpenses,weeklyExpenses,monthlyExpenses
+    }
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
       const [userData, setUserData] = useState({
-        labels: UserData.map((data) => data.year),
+        labels: expenses.map((expense) => expense.category),
         datasets: [
           {
+            title: {
+                display: true,
+                text: 'Custom Chart Title'
+            },
             label: "Users Gained",
-            data: UserData.map((data) => data.userGain),
+            data: expenses.map((expense) => expense.amount),
             backgroundColor: [
-              "rgba(75,192,192,1)",
-              "#ecf0f1",
-              "#50AF95",
-              "#f3ba2f",
-              "#2a71d0",
+              ...expenses.map(ex=>getRandomColor())
             ],
             borderColor: "black",
             borderWidth: 2,
           },
         ],
       });
-  return <>
-        <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
-        <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-        <Pie data={userData} />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-            <button>close</button>
-        </form>
-        </dialog>
-    </>
+
+      function getData(expenses){
+          return {
+            labels: expenses.map((expense) => expense.category),
+            datasets: [
+              {
+                title: {
+                    display: true,
+                    text: 'Custom Chart Title'
+                },
+                label: "Users Gained",
+                data: expenses.map((expense) => expense.amount),
+                backgroundColor: [
+                  ...expenses.map(ex=>getRandomColor())
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          }
+      }
+
+      function getOptions(title){
+          return {
+            plugins: {
+                title: {
+                display: true,
+                text: title,
+                align: "center",
+                },
+            },
+            }
+      }
+  return <div className="flex gap-5 px-10 justify-center flex-wrap">
+        {
+            Object.keys(Expenses).map(key=>{
+                return <div key={key}>
+                    <button className="btn" onClick={()=>document.getElementById(key).showModal()}>show {key}</button>
+                    <dialog id={key} className="modal">
+                        <div className="modal-box text-center" >
+                            <Pie options={getOptions(key)} data={getData(Expenses[key])} />
+                        </div>
+                        <form method="dialog" className="modal-backdrop">
+                            <button>close</button>
+                        </form>
+                    </dialog>
+                </div>
+            })
+        }
+    </div>
 }
 
 export default PieChart;
+
+
+
+
+
+
+
+
+
+/* 
+import React from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+ 
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+ 
+export default function Chart({expenses}) {
+
+    const Expenses = {
+        totalExpenses : expenses,
+        dailyExpenses : expenses.filter(expense=>expense.period=='daily'),
+        weeklyExpenses : expenses.filter(expense=>expense.period=='weekly'),
+        monthlyExpenses : expenses.filter(expense=>expense.period=='monthly')
+    }
+    const dailyExpenses = expenses.filter(expense=>expense.period=='daily');
+    const weeklyExpenses = expenses.filter(expense=>expense.period=='weekly');
+    const monthlyExpenses = expenses.filter(expense=>expense.period=='monthly');
+
+    function getDataPoinst(expenses){
+        let totalAmount = getTotalAmount(expenses);
+        return expenses.map(expense=>{
+            console.log(typeof expense.amount,totalAmount);
+            let y = Math.floor((expense.amount/totalAmount)*100);
+            console.log(y)
+            return { y  ,label : expense.category};
+        })
+    }
+
+    function getTotalAmount(expenses){
+        let amount = 0;
+        expenses.map(expense=>{
+            amount += expense.amount;
+        });
+        return amount;
+    }
+    const dataPointsForTotalCost = getDataPoinst(expenses);
+    const dataPointsForDailyExpenses = getDataPoinst(dailyExpenses);
+    const dataPointsForWeeklyExpenses = getDataPoinst(weeklyExpenses);
+    const dataPointsForMonthlyExpenses = getDataPoinst(monthlyExpenses);
+
+    function getOptions(dataPoints,text){
+        return {
+            animationEnabled: true,
+            exportEnabled: true,
+            theme: "dark1", // "light1", "dark1", "dark2"
+            title:{
+                text
+            },
+            data: [{
+                type: "pie",
+                indexLabel: "{label}: {y}%",		
+                startAngle: -90,
+                dataPoints
+            }]
+        }
+    }
+    const options = {
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "dark1", // "light1", "dark1", "dark2"
+        title:{
+            text: "Expenses"
+        },
+        data: [{
+            type: "pie",
+            indexLabel: "{label}: {y}%",		
+            startAngle: -90,
+            dataPoints:dataPointsForTotalCost
+        }]
+    }
+    
+    return (
+    <div className="flex flex-wrap justify-center gap-5">
+
+        <div>
+            <button className="btn" onClick={()=>document.getElementById('showTotalExpenses').showModal()}>showTotalExpenses</button>
+            <dialog id="showTotalExpenses" className="modal">
+                <div className="modal-box">
+                    <CanvasJSChart options = {getOptions(dataPointsForTotalCost,"Total Expenses")} 
+                    /* onRef={ref => this.chart = ref} 
+                    />
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+
+        <div>
+            <button className="btn" onClick={()=>document.getElementById('showDailyExpenses').showModal()}>showDailyExpenses</button>
+            <dialog id="showDailyExpenses" className="modal">
+                <div className="modal-box">
+                <CanvasJSChart options = {getOptions(dataPointsForDailyExpenses,"Daily Expenses")} 
+                    /* onRef={ref => this.chart = ref} 
+                />
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+        <div>
+            <button className="btn" onClick={()=>document.getElementById('showWeeklyExpenses').showModal()}>showWeeklyExpenses</button>
+            <dialog id="showWeeklyExpenses" className="modal">
+                <div className="modal-box">      
+                    <CanvasJSChart options = {getOptions(dataPointsForWeeklyExpenses,"Weekly Expenses")} 
+                        /* onRef={ref => this.chart = ref} 
+                    />
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+        <div>
+            <button className="btn" onClick={()=>document.getElementById('showMonthlyExpenses').showModal()}>showMonthlyExpenses</button>
+            <dialog id="showMonthlyExpenses" className="modal">
+                <div className="modal-box">
+                <CanvasJSChart options = {getOptions(dataPointsForMonthlyExpenses,"Monthly Expenses")} 
+                    /* onRef={ref => this.chart = ref} 
+                />
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+
+        
+
+        
+    </div>
+    );
+}
+
+*/
